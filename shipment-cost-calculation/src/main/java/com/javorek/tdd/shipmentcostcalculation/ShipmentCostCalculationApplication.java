f335367e4b7669b415db250a26d5aa412c48211f
Pawel <pawel.javorski@gmail.com>
2022-04-23 17:56:16 +0200

    shipment cost calculation

---
+++ shipment-cost-calculation/src/main/java/com/javorek/tdd/shipmentcostcalculation/ShipmentCostCalculationApplication.java
@@ -0,0 +1,13 @@
+package com.javorek.tdd.shipmentcostcalculation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipmentCostCalculationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShipmentCostCalculationApplication.class, args);
+	}
+
+}
---
+++ shipment-cost-calculation/src/test/java/com/javorek/tdd/shipmentcostcalculation/ShipmentCostCalculationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javorek.tdd.shipmentcostcalculation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShipmentCostCalculationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
