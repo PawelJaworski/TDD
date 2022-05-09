258cab2abf5364f2473ae6806cfcfe88caa8175d
Pawel <pawel.javorski@gmail.com>
2022-05-09 21:22:32 +0200

    how to integration test

---
+++ how-not-to/src/test/java/com/javorek/tdd/howto/HowToIntegrationTest.java
@@ -0,0 +1,44 @@
+package com.javorek.tdd.howto;
+
+import com.javorek.tdd.hownotto.HowNotToApplication;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(classes = HowNotToApplication.class)
+class HowToIntegrationTest {
+
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+
+	private MockMvc mockMvc;
+
+	@BeforeEach
+	public void setup() {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
+	}
+
+	@Test
+	@SneakyThrows
+	void shouldReturnHttp400WhenIncorrectInput() {
+		var json = "{\"first\":-1,\"operation\":\"additamentum\",\"second\":-1}";
+		this.mockMvc.perform(post("/")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(json)
+				.characterEncoding("utf-8"))
+				.andDo(print())
+				.andExpect(status().isBadRequest())
+				.andReturn();
+	}
+}
